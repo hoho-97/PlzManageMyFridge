ddcfe60507641bd14ad44447b7e8eb7279593f10
hallu0317 <hallu0317@gmail.com>
2021-06-03 14:57:21 +0900

    김지원

--- app/src/main/java/com/example/nengzanggo2/MainActivity.kt
+++ app/src/main/java/com/example/nengzanggo2/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.nengzanggo2
-
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var img3 : ImageButton
+        img3 = findViewById(R.id.imageButton3)
+
+        img3.setOnClickListener{
+            var intent = Intent(applicationContext,OrderActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
