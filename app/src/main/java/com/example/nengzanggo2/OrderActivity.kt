d69c055290c9de5832650b32804032e125f8bc67
Bonghee <hae8064@naver.com>
2021-06-06 19:02:40 +0900

    음성인식

--- app/src/main/java/com/example/nengzanggo2/OrderActivity.kt
+++ app/src/main/java/com/example/nengzanggo2/OrderActivity.kt
@@ -4,12 +4,16 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.view.Gravity
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import android.widget.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.order.*
+
+//음성인식 부분에 필요한거
+private const val SPEECH_REQUEST_CODE = 0
 
 class OrderActivity : Activity() {
 
@@ -20,14 +24,22 @@ class OrderActivity : Activity() {
         title="주문!!"
         var Edit1 : EditText
         var btnSearch : Button
+        var btnMic : Button
+        var text : String
 
         Edit1 = findViewById(R.id.Edit1)
         btnSearch = findViewById(R.id.btnSearch)
+        btnMic = findViewById(R.id.btnMic)
 
         btnSearch.setOnClickListener {
-            var text = Edit1.text.toString()
+            text = Edit1.text.toString()
             var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.coupang.com/np/search?component=&q=${text}&channel=user/"))
             startActivity(intent)
+        }
+
+        //음성인식 버튼
+        btnMic.setOnClickListener {
+            displaySpeechRecognizer()
         }
 
         val titleArr = arrayOf("된장찌개", "김치찌개", "순두부찌개", "제육볶음")
@@ -88,5 +100,27 @@ class OrderActivity : Activity() {
             }
             true
         }
+        
+        
+        //음성인식 부분
+    }
+    private fun displaySpeechRecognizer() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        }
+        // Start the activity, the intent will be populated with the speech text
+        startActivityForResult(intent, SPEECH_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val spokenText: String? =
+                data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS).let { results ->
+                    results?.get(0)
+                }
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.coupang.com/np/search?component=&q=${spokenText}&channel=user/"))
+            startActivity(intent)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
